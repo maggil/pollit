dcb006c2c71e8b224add5df918851c2684ed2261
maggil <maggi@kamnet.com>
2014-04-12 11:58:01 -0700

    Simple sliceable views
    That’s it. (five)

---
+++ app/src/main/java/com/pollit/android/app/FriendFragment.java
@@ -0,0 +1,8 @@
+package com.pollit.android.app;
+
+/**
+ * Created by m on 4/12/14.
+ */
+public class FriendFragment {
+
+}
--- app/src/main/java/com/pollit/android/app/MainActivity.java
+++ app/src/main/java/com/pollit/android/app/MainActivity.java
@@ -13,10 +13,18 @@ import android.support.v4.view.ViewPager;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.content.Context;
+import android.widget.Toast;
+import android.support.v7.app.ActionBar.Tab;
+import android.R.id;
+
+
 
 
 public class MainActivity extends ActionBarActivity {
@@ -31,6 +39,8 @@ public class MainActivity extends ActionBarActivity {
      */
     SectionsPagerAdapter mSectionsPagerAdapter;
 
+
+
     /**
      * The {@link ViewPager} that will host the section contents.
      */
@@ -41,15 +51,72 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //Set ActionBar attributes.
+        ActionBar ab = getSupportActionBar();
+        //ab.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        ab.setDisplayShowTitleEnabled(false);
+        ab.setDisplayShowHomeEnabled(false);
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
+        // lets you swipe across screens
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
+        /*mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                ActionBar a = getSupportActionBar();
+                if(position == 0)
+                {
+                    a.removeAllTabs();
+                    Tab tab1 = a.newTab();
+                    tab1.setTabListener(new MyTabsListener());
+                    tab1.setText("FRIENDS");
+                    tab1.setContentDescription("Friends");
+                    a.addTab(tab1);
+                    Tab tab2 = a.newTab();
+                    tab2.setTabListener(new MyTabsListener());
+                    tab2.setText("+");
+                    tab2.setContentDescription("FriendsBox");
+                    a.addTab(tab2);
+                    a.show();
+                }
+                else if (position == 1)
+                    a.hide();
+                else if (position == 2)
+                {
+                    a.removeAllTabs();
+                    Tab tab1 = a.newTab();
+                    tab1.setTabListener(new MyTabsListener());
+                    tab1.setText("FRIENDS");
+                    a.addTab(tab1);
+                    Tab tab2 = a.newTab();
+                    tab2.setTabListener(new MyTabsListener());
+                    tab2.setText("PUBLIC");
+                    tab2.setContentDescription("Public");
+                    a.addTab(tab2);
+                    a.show();
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });*/
+
+        //generate fragments.
+
+
         mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setCurrentItem(2);
 
     }
 
@@ -91,12 +158,13 @@ public class MainActivity extends ActionBarActivity {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
             return PlaceholderFragment.newInstance(position + 1);
+
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 5;
         }
 
         @Override
@@ -109,10 +177,17 @@ public class MainActivity extends ActionBarActivity {
                     return getString(R.string.title_section2).toUpperCase(l);
                 case 2:
                     return getString(R.string.title_section3).toUpperCase(l);
+                case 3:
+                    return getString(R.string.title_section4).toUpperCase(l);
+                case 4:
+                    return getString(R.string.title_section5).toUpperCase(l);
             }
             return null;
         }
     }
+
+   
+    //}
 
     /**
      * A placeholder fragment containing a simple view.
@@ -145,8 +220,61 @@ public class MainActivity extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
+                case 0:
+
+            }
+           // LinearLayout lin = (LinearLayout) rootView.findViewById(R.id.stuff);
             return rootView;
         }
     }
+
+    /*class MyTabsListener implements ActionBar.TabListener {
+        public Fragment fragment;
+        public Context context;
+        ViewPager vp = (ViewPager) findViewById(R.id.pager);
+
+        @Override
+        public void onTabReselected(Tab tab, FragmentTransaction ft) {
+
+        }
+
+        @Override
+        public void onTabSelected(Tab tab, FragmentTransaction ft) {
+            TextView textView = (TextView) findViewById(R.id.section_label);
+
+            //List of friends
+            if(tab.getContentDescription() == "Friends")
+            {
+                textView.setText("Hello Friends!");
+
+                //ft.replace(R.id.fragment_container, fragment.getTargetFragment());
+            }
+            //Inbox
+            else if (tab.getContentDescription() == "FriendsBox")
+            {
+                textView.setText("Hello Meeper!");
+                //ft.replace(R.id.fragment_container, fragment.getTargetFragment());
+            }
+            //Public Inbox
+            else if (tab.getContentDescription() == "Public")
+            {
+                textView.setText("Creeper!");
+                //ft.replace(R.id.fragment_container, fragment.getTargetFragment());
+            }
+            //Add friends tab.
+            else
+            {
+                textView.setText("Kinda cool");
+                //ft.replace(R.id.fragment_container, fragment.getTargetFragment());
+            }
+        }
+
+        @Override
+        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+
+        }
+
+    }*/
 
 }
